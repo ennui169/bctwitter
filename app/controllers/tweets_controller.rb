a5c51578120566d1adbdd47d3085825bac28fd8e
unknown <pongv@bc.edu>
2015-01-24 18:00:45 -0500

    tweet attached to user, logged in to tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,19 @@
 class TweetsController < ApplicationController
 
+	before_action :authenticate_user!
 	def new 
 		@tweet = Tweet.new
 
 	end
 
 	def create
-		@tweet = Tweet.create(tweet_params)
+
+		@tweet = Tweet.new(tweet_params)
+		@tweet.user = current_user
+		@tweet.save
+
+
+		#@tweet = Tweet.create(tweet_params)
 		flash.now[:success] = "Tweet Created"
 		render 'new'
 	end
