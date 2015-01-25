eefa7e0bfe3e0b5fc5d9b643089b4d984432d3cd
unknown <pongv@bc.edu>
2015-01-25 14:03:17 -0500

    added user profile with pic and name

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
 	def index
+		if user_signed_in?
+			if current_user.profile #if it exists, it returns the profile; if it doesn't, it returns new
+				redirect_to tweets_path #redirecting to control file--try to use redirect, less render
+			else
+				flash[:success] = "Create a profile below" #flash survives a redirect--other variables lost; when rendering, it is not redirecting, add .now to say don't survive redirect
+				redirect_to new_profile_path
+			end
+		end
 	end
 end
--- db/migrate/20150124223502_add_user_id_to_tweets.rb
+++ db/migrate/20150124223502_add_user_id_to_tweets.rb
@@ -1,4 +1,4 @@
-class AddUserIdToTweets < ActiveRecord::Migration
+class AddUserIdToTweets < ActiveRecord::Migrationprofi
   def change
     add_reference :tweets, :user, index: true
   end
